70b8d51a69e536a006ffa91f43e7f9457e9095e9
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-01-06 17:56:59 +0600

    Update test_case_3.py

--- test_case_3.py
+++ test_case_3.py
@@ -1,7 +1,23 @@
 import unittest
 
-
 class AppTesting(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        print("This is login test")
+
+    @classmethod
+    def tearDown(self):
+        print("This is logout test")
+
+    @classmethod
+    def setUpClass(cls):
+        print("Open Application")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("Close Application")
+
     def test_search(self):
         print('This is search test')
 
@@ -12,8 +28,7 @@ class AppTesting(unittest.TestCase):
         print("This is prepaid recharge test")
 
     def test_postpaid_recharge(self):
-        print("This is post paid recharge test")
-
+        print("This is post-paid recharge test")
 
 if __name__ == "__main__":
     unittest.main()
