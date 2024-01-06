1dc30dd798a4d96f35c24196d8221d4cbf8c0715
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-01-06 18:11:55 +0600

    Update test_case_3.py

--- test_case_3.py
+++ test_case_3.py
@@ -1,21 +1,30 @@
 import unittest
 
+
+def setUpModule():  # will be executed before executing any class or any method present in the test class
+    print("setUpModule")
+
+
+def tearDownModule():  # will be executed after completing everything present in the python module
+    print('tearDownModule')
+
+
 class AppTesting(unittest.TestCase):
 
     @classmethod
-    def setUp(self):
+    def setUp(self):  # execute before all test methods
         print("This is login test")
 
     @classmethod
-    def tearDown(self):
+    def tearDown(self):  # Execute after all test methods
         print("This is logout test")
 
     @classmethod
-    def setUpClass(cls):
+    def setUpClass(cls):  # Execute once when the class started
         print("Open Application")
 
     @classmethod
-    def tearDownClass(cls):
+    def tearDownClass(cls):  # Execute once after the class completed
         print("Close Application")
 
     def test_search(self):
@@ -29,6 +38,7 @@ class AppTesting(unittest.TestCase):
 
     def test_postpaid_recharge(self):
         print("This is post-paid recharge test")
+
 
 if __name__ == "__main__":
     unittest.main()
